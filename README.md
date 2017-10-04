b1e33617a9c1d694432402d0b312095f4e5c4ad0
Rebecca <rebecca.kane@outlook.com>
2017-10-04 21:15:17 +0100

    Trying out a basic app, xaml code done, azure set up online

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MobileApps3Project
-This is a project for 4th Year Software Development module Mobile Apps Development 3.
+> Module: Mobile Applications Development 3  
+> Lecturer: Damien Costello  
+> Project: Create a UWP app with responsive UI across multiple devices. Use mobile services for data storage and retrieval.  
+
+[Project Spec PDF](https://learnonline.gmit.ie/pluginfile.php/187530/mod_resource/content/0/Mobile%20Applications%20Development%202%20-%20Project.pdf)
---
+++ TestApp/TestApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+/* This is a test project, just using this to practise connecting to Azure etc.
+* Will delete and replace with actual project when done. Basic app adapted from tutorial at
+* http://www.c-sharpcorner.com/article/how-to-add-azure-mobile-app-to-an-uwp-app/ */
+
+namespace TestApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
