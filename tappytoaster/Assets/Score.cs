d89143059c331071cb0b932bc15f26a25a6ea0db
Rebecca <rebecca.kane@outlook.com>
2017-12-05 20:30:25 +0000

    Working on scoring

---
+++ tappytoaster/Assets/Score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    static int score = 3;
+    static int highScore = 15;
+    public Text PlayerScore;
+
+    static public void addScore() {
+        score++;
+
+        if (score > highScore)
+            highScore = score;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        PlayerScore.text = "SCORE: " + score + "\nBEST: " + highScore;
+    }
+}
